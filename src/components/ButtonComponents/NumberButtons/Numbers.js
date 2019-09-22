02a066177c7e8ce6bdbdc7671223f1bea893f27e
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-09-22 03:10:44 -0400

    Import the data found in data.js into Numbers.js, Operators.js, and Specials.js. Add the imported data to state.

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { numbers } from "../../../data";
 
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
+// example of import from data.js. Note all the ../   This is how we move through folders.
 /* 
 import { numbers } from '../../../data' 
 */
@@ -9,6 +10,7 @@ import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const { numbersState, setNumbersState } = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
